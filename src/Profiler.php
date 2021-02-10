085bd4730e2f010ce6b0a8826824605ca3936a90
lozovoyv <lozovoyv@gmail.com>
2021-02-10 20:23:17 +0300

    Refactor to interface change

--- src/Profiler.php
+++ src/Profiler.php
@@ -14,40 +14,20 @@ use OpxCore\App\Interfaces\ProfilerInterface;
 
 class Profiler implements ProfilerInterface
 {
-    /** @var Profiler instance */
-    protected static Profiler $profiler;
-
     /** @var array Profiling application */
     protected array $profiling = [];
 
     /** @var int External timestamp of profiling start */
-    protected int $profilingStartTime;
+    protected int $startTime;
 
     /** @var int External memory usage of profiling start */
-    protected int $profilingStartMem;
+    protected int $startMemory;
 
     /** @var array Timestamp of application start */
-    protected array $profilingStopWatches = [];
+    protected array $actions = [];
 
     /** @var bool Is profiling enabled */
-    protected bool $profilingEnabled = true;
-
-    /**
-     * Create og get profiler singleton.
-     *
-     * @param int|null $startTime
-     * @param int|null $startMem
-     *
-     * @return  Profiler
-     */
-    public static function getProfiler(?int $startTime = null, ?int $startMem = null): Profiler
-    {
-        if (self::$profiler === null) {
-            self::$profiler = new self($startTime, $startMem);
-        }
-
-        return self::$profiler;
-    }
+    protected bool $enabled = true;
 
     /**
      * Profiler constructor.
@@ -59,9 +39,8 @@ class Profiler implements ProfilerInterface
      */
     public function __construct(?int $startTime = null, ?int $startMem = null)
     {
-        $this->profilingStartTime = $startTime ?? hrtime(true);
-        $this->profilingStartMem = $startMem ?? memory_get_usage();
-        self::$profiler = $this;
+        $this->startTime = $startTime ?? hrtime(true);
+        $this->startMemory = $startMem ?? memory_get_usage();
     }
 
     /**
@@ -71,32 +50,32 @@ class Profiler implements ProfilerInterface
      *
      * @return  void
      */
-    public function profilingStart(string $action): void
+    public function start(string $action): void
     {
-        if (!$this->profilingEnabled) {
+        if (!$this->enabled) {
             return;
         }
 
-        $this->profilingStopWatches[$action] = hrtime(true);
+        $this->actions[$action] = hrtime(true);
     }
 
     /**
      * Write action to profiling or get whole profiling list.
      *
-     * @param string|null $action
+     * @param string $action
      * @param int|null $timestamp
      * @param int|null $memory
      *
      * @return  void
      */
-    public function profilingStop(?string $action = null, ?int $timestamp = null, ?int $memory = null): void
+    public function stop(string $action, ?int $timestamp = null, ?int $memory = null): void
     {
-        if ($this->profilingEnabled === false) {
+        if ($this->enabled === false) {
             return;
         }
         $now = (int)hrtime(true);
-        $executionTime = array_key_exists($action, $this->profilingStopWatches) ? ($now - $this->profilingStopWatches[$action]) : null;
-        $timeStamp = $timestamp ?? ($now - $this->profilingStartTime - $executionTime ?? 0);
+        $executionTime = array_key_exists($action, $this->actions) ? ($now - $this->actions[$action]) : null;
+        $timeStamp = $timestamp ?? ($now - $this->startTime - $executionTime ?? 0);
         $stack = debug_backtrace(0);
         // Exclude profilingStop() function call from stacktrace
         array_shift($stack);
@@ -109,34 +88,37 @@ class Profiler implements ProfilerInterface
             'stack' => $stack,
         ];
 
-        unset($this->profilingStopWatches[$action]);
+        unset($this->actions[$action]);
     }
 
     /**
      * Returns profiling list or set profiling mode enabled or disabled.
      *
-     * @param bool|null $enable
-     *
      * @return  array[]|null
      */
-    public function profiling(?bool $enable = null): ?array
+    public function profiling(): ?array
     {
-        // If parameter is bool set profiling mode
-        if (is_bool($enable)) {
-            $this->profilingEnabled = $enable;
-
+        if (!$this->enabled) {
             return null;
         }
 
-        if (!$this->profilingEnabled) {
-            return null;
-        }
-
-        // Order profiled items by timestamp first
+        // Order profiled items by timestamp
         usort($this->profiling, static function ($a, $b) {
             return $a['timestamp'] <=> $b['timestamp'];
         });
 
         return $this->profiling;
+    }
+
+    /**
+     * Enable or disable profiling.
+     *
+     * @param bool $enable
+     *
+     * @return  void
+     */
+    public function enable(bool $enable = true): void
+    {
+        $this->enabled = $enable;
     }
 }
